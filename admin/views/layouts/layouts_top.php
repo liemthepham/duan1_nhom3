583c533689567cc729e51a91750812a09d03ce08
liemthepham <liemptph37724@fpt.edu.vn>
2025-05-20 20:10:06 +0700

    main

---
+++ admin/views/layouts/layouts_top.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg" data-sidebar-image="none" data-preloader="disable" data-theme="default" data-theme-colors="default">
+
+<head>
+
+    <meta charset="utf-8" />
+    <title>Dashboard | NN Shop</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesbrand" name="author" />
+
+    <!-- CSS -->
+    <?php
+    require_once "./views/layouts/libs_css.php";
+    ?>
+
+</head>
+
+<body>
+
+    <!-- Begin page -->
+    <div id="layout-wrapper">
+
+        <!-- HEADER -->
+        <?php
+        require_once "./views/layouts/header.php";
+
+        require_once "./views/layouts/siderbar.php";
+        ?>
+        
+        <!-- Left Sidebar End -->
+        <!-- Vertical Overlay-->
+        <div class="vertical-overlay"></div>
+
+        <!-- ============================================================== -->
+        <!-- Start right Content here -->
+        <!-- ============================================================== -->
+        <div class="main-content">
+
+            <div class="page-content">
+                <div class="container-fluid"></div>
