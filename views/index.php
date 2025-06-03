0e706c11a73903ba16845b56f100c4ebd488f0ce
Tên của bạn <email@example.com>
2025-06-04 02:24:24 +0700

    "css"

--- views/index.php
+++ views/index.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
-    <link rel="stylesheet" href="/duan1//public/css/cusstom.css">
+    <link rel="stylesheet" href="public/css/cusstom.css">
     <style>
 
     </style>
